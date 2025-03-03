574b12acad12134bb40fbb4896d53ad86640a7c2
Leticia Cosmo <letcosmogeller@gmail.com>
2025-03-03 18:24:54 -0300

    first commit

---
+++ src/layout/logo/Logo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import LogoLight2x from "../../images/logo-horizontal-sem-fundo2x.png";
+import LogoDark2x from "../../images/logo-horizontal-sem-fundo2x.png";
+import {Link} from "react-router-dom";
+
+const Logo = () => {
+  return (
+    // <Link to={`${process.env.PUBLIC_URL}/`} className="logo-link">
+    <Link to={`user-list-regular`} className="logo-link">
+      <img className="logo-light logo-img" src={LogoLight2x} alt="logo" />
+      <img className="logo-dark logo-img" src={LogoDark2x} alt="logo" />
+    </Link>
+  );
+};
+
+export default Logo;
